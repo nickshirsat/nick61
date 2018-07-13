d38108a6baee41557948d917a2136e048d43b7e7
Nikhil Shirsat <40695524+nickshirsat@users.noreply.github.com>
2018-07-13 11:32:58 +0530

    Update date1.py

--- date1.py
+++ date1.py
@@ -1,20 +1,25 @@
-import datetime
+import  datetime 
 
-t = datetime.time(1,2,3)
+t = datetime.datetime(1,2,3)
+now1 = datetime.datetime.now()
 
-print (t)
-
+print('user time: ',t)
+print('\nSystem time ',now1)
 print('Hours: ', t.hour)
 print('Minutes: ', t.minute)
 print('Seconds: ', t.second)
 print('Tzinfo: ', t.tzinfo);
 
 
+
+
 ''' OUTPUT
 
-01:02:03
-Hours:  1
-Minutes:  2
-Seconds:  3
+user time:  0001-02-03 00:00:00
+
+System time  2018-07-13 11:32:13.147310
+Hours:  0
+Minutes:  0
+Seconds:  0
 Tzinfo:  None
 '''
