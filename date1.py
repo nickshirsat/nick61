8caaa0e06db5613b9b870b6867566160a279d69f
Nikhil Shirsat <40695524+nickshirsat@users.noreply.github.com>
2018-07-13 11:02:55 +0530

    Add files via upload

---
+++ date1.py
@@ -0,0 +1,20 @@
+import datetime
+
+t = datetime.time(1,2,3)
+
+print (t)
+
+print('Hours: ', t.hour)
+print('Minutes: ', t.minute)
+print('Seconds: ', t.second)
+print('Tzinfo: ', t.tzinfo);
+
+
+''' OUTPUT
+
+01:02:03
+Hours:  1
+Minutes:  2
+Seconds:  3
+Tzinfo:  None
+'''
